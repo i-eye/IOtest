f668ea2c3e893acff921a7a7be3a9e12452d75ca
i-eye <ivan.freeman042@gmail.com>
2022-02-18 15:41:34 -0600

    gaming

--- src/com/WinstonStatue/Main.java
+++ src/com/WinstonStatue/Main.java
@@ -1,27 +1,35 @@
 package com.WinstonStatue;
+//imports for writing to a file
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
+//imports for reading a file
 import java.util.Scanner;
 import java.io.FileInputStream;
+
+//IO exception, probably shouldn't matter on the competition
+import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    FileOutputStream outputStream;
-        PrintWriter fileOut;
-        outputStream = new FileOutputStream("gaming.txt");
-        fileOut = new PrintWriter(outputStream);
+        //output variables
+	    FileOutputStream outputStream = new FileOutputStream("gaming.txt");
+        PrintWriter fileOut = new PrintWriter(outputStream);
 
+        //prints a line to gaming.txt then closes it
         fileOut.println("Hello world");
         fileOut.close();
 
+        //input variables
         FileInputStream inputStream = new FileInputStream("laming.txt");
-
         Scanner fileIn = new Scanner(inputStream);
+
+        //while laming.txt has another line, it prints it out
         while(fileIn.hasNextLine()) {
             System.out.println(fileIn.nextLine());
         }
+        //closes fileIn
+        fileIn.close();
 
     }
 }
