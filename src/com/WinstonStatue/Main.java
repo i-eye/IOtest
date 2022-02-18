97b998c9e630b5fd8acecba07079a7bd565c75ce
I_Eye <ivan.freeman042@gmail.com>
2022-02-18 15:27:57 -0600

    Initial commit

---
+++ src/com/WinstonStatue/Main.java
@@ -0,0 +1,27 @@
+package com.WinstonStatue;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    FileOutputStream outputStream = null;
+        PrintWriter fileOut = null;
+        outputStream = new FileOutputStream("gaming.txt");
+        fileOut = new PrintWriter(outputStream);
+
+        fileOut.println("Hello world");
+        fileOut.close();
+
+        FileInputStream inputStream = new FileInputStream("laming.txt");
+
+        Scanner fileIn = new Scanner(inputStream);
+        while(fileIn.hasNextLine()) {
+            System.out.println(fileIn.nextLine());
+        }
+
+    }
+}
